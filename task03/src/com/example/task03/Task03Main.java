d8d93c9e00ae2932a2c940c9df6c6d9a3f297dc6
Alexey Postnov <adpostnov@yandex.ru>
2024-10-22 23:05:44 +0500

    Постнов Алексей. Задание 03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,7 @@
 package com.example.task03;
 
 public class Task03Main {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         throwCheckedException();
     }
 
